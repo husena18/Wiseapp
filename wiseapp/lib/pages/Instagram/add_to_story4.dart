1a6a10f4dc94ffef8d39a5403fc796e0dc5f62bc
husena18 <husena.limdiwala@nuv.ac.in>
2024-03-09 00:24:55 +0530

    .

--- wiseapp/lib/pages/Instagram/add_to_story4.dart
+++ wiseapp/lib/pages/Instagram/add_to_story4.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:lottie/lottie.dart';
 import 'package:wiseapp/image_fetch.dart';
+import 'package:wiseapp/pages/Instagram/Insta_main.dart';
+
 
 class Addtostory4IgWidget extends StatefulWidget {
   const Addtostory4IgWidget({Key? key}) : super(key: key);
@@ -12,52 +13,52 @@ class Addtostory4IgWidget extends StatefulWidget {
 class _Addtostory4IgWidgetState extends State<Addtostory4IgWidget> {
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {},
-      child: Scaffold(
-        backgroundColor: Colors.white, // Change to the desired background color
-        body: SafeArea(
-          top: true,
-          child: Stack(
-            children: [
-              Align(
-                alignment: AlignmentDirectional.center,
-                child: Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(8),
-                    child: ImageFetcher(
-                      imageUrl: 'instagram_assets/WhatsApp_Image_2024-02-28_at_23.25.39_(1).jpeg',
-                    ),
+    return Scaffold(
+      backgroundColor: Colors.white, // Change to the desired background color
+      body: SafeArea(
+        top: true,
+        child: Stack(
+          children: [
+            Align(
+              alignment: AlignmentDirectional.center,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: ImageFetcher(
+                    imageUrl: 'instagram_assets/WhatsApp_Image_2024-02-28_at_23.25.39_(1).jpeg',
                   ),
                 ),
               ),
-              Align(
-                alignment: AlignmentDirectional(-1.57, -0.92),
-                child: Lottie.asset(
-                    'animations/cursor.json',
-                    width: 250,
-                    height: 220,
-                    reverse: true,
-                    repeat: true,
-                    fit: BoxFit.fitWidth,
-                    animate: true,
+            ),
+            Align(
+              alignment: AlignmentDirectional(0, -0.1),
+              child: Text(
+                'Story uploaded',
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white, // Change to the desired text color
                 ),
               ),
-              Align(
-                alignment: AlignmentDirectional(-0.9, -0.6),
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => InstaMain()), // Replace YourNextPage with the actual next page widget
+                  );
+                },
                 child: Text(
-                  'Story uploaded',
-                  style: TextStyle(
-                    fontSize: 28,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white, // Change to the desired text color
-                  ),
+                  "Let's explore other functionalities",
+                  style: TextStyle(fontSize: 18),
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
