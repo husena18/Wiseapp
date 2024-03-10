34f71e2d721db450c1626abdd6850ffbf4f7c480
Sush893 <suhani12345singh@gmail.com>
2024-03-11 00:28:25 +0530

    add

--- wiseapp/lib/pages/Instagram/add_to_story1.dart
+++ wiseapp/lib/pages/Instagram/add_to_story1.dart
@@ -18,7 +18,7 @@ class _Addtostory1IgWidgetState extends State<Addtostory1IgWidget> {
   Widget build(BuildContext context) {
     return Scaffold(
       key: scaffoldKey,
-      backgroundColor: Colors.grey[200],
+      backgroundColor: Theme.of(context).backgroundColor,
       body: SafeArea(
         top: true,
         child: Stack(
@@ -30,47 +30,71 @@ class _Addtostory1IgWidgetState extends State<Addtostory1IgWidget> {
                 height: MediaQuery.of(context).size.height,
                 child: ClipRRect(
                   child: ImageFetcher(
-                    imageUrl: 'instagram_assets/WhatsApp_Image_2024-02-28_at_23.25.40_(1).jpeg',
+                    imageUrl: 'instagram_assets//WhatsApp_Image_2024-02-28_at_23.28.03.jpeg',
                   ),
                 ),
               ),
             ),
             GestureDetector( // Wrap Lottie.asset with GestureDetector
-              onTap: () {
-                Navigator.push( // Navigate to another page when animation is clicked
-                  context,
-                  MaterialPageRoute(builder: (context) => Addtostory2IgWidget()), // Replace DestinationPage with your destination page
-                );
-              },
-              child: Opacity(
-                opacity: 0.8,
-                child: Align(
-                  alignment: AlignmentDirectional(0.3, 0.43),
-                  child: Lottie.asset(
-                    'animations/cursor.json',
-                    width: 250,
-                    height: 220,
-                    reverse: true,
-                    repeat: true,
-                    fit: BoxFit.fitWidth,
-                    animate: true,
+  onTap: () {
+    // Navigate to another page when animation is clicked
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => Addtostory2IgWidget()),
+    );
+  },
+  child: Opacity(
+    opacity: 0.8,
+    child: Align(
+      alignment: AlignmentDirectional(-1.38, -0.96),
+      child: Lottie.asset(
+        'animations/cursor.json',
+        width: 250,
+        height: 220,
+        reverse: true,
+        repeat: true,
+        fit: BoxFit.fitWidth,
+        animate: true,
+      ),
+    ),
+  ),
+),
+
+            Opacity(
+              opacity: 0,
+              child: Align(
+                alignment: AlignmentDirectional(-0.97, -0.79),
+                child: InkWell(
+                  splashColor: Colors.transparent,
+                  focusColor: Colors.transparent,
+                  hoverColor: Colors.transparent,
+                  highlightColor: Colors.transparent,
+                  onTap: () async {
+                    // Handle onTap
+                  },
+                  child: Icon(
+                    Icons.settings_outlined,
+                    color: Theme.of(context).textTheme.bodyText1!.color,
+                    size: 100,
                   ),
                 ),
               ),
             ),
             Align(
-              alignment: AlignmentDirectional(0.01, -0.79),
+              alignment: AlignmentDirectional(-0.83, -0.53),
               child: InkWell(
-                onTap: () async {
-                  // Handle onTap
-                },
+                splashColor: Colors.transparent,
+                focusColor: Colors.transparent,
+                hoverColor: Colors.transparent,
+                highlightColor: Colors.transparent,
+                onTap: () async {},
                 child: Text(
-                  AppLocalizations.of(context)!.fromgallery,
+                  AppLocalizations.of(context)!.clickhere,
                   textAlign: TextAlign.center,
-                  style: TextStyle(
+                  style: Theme.of(context).textTheme.bodyText1!.copyWith(
                     fontFamily: 'Readex Pro',
-                    color: Colors.white,
-                    fontSize: 22,
+                    color: Colors.white, // Change text color to white
+                    fontSize: 26,
                   ),
                 ),
               ),
@@ -79,6 +103,5 @@ class _Addtostory1IgWidgetState extends State<Addtostory1IgWidget> {
         ),
       ),
     );
-
   }
 }
